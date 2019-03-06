61f3878287a8c046e1a79a5cdbae7c5c07d6f63b
Guillem Ortega <guillem212@hotmail.es>
2019-03-06 14:50:07 +0100

    Cosas

--- Project Stealth/Assets/Scripts/colliderCamera.cs
+++ Project Stealth/Assets/Scripts/colliderCamera.cs
@@ -9,6 +9,13 @@ public class colliderCamera : MonoBehaviour
 
     private Rigidbody rb_Camera;
 
+    //Punto en el que impacta la camara.
+    private ContactPoint point;
+    public GameObject virtualCamera;
+
+    private float initialCameraRotation;
+    public static float min, max;
+
     private void Start()
     {
         rb_Camera = GetComponent<Rigidbody>();
@@ -19,12 +26,29 @@ public class colliderCamera : MonoBehaviour
         if (collision.gameObject.tag == "Wall")
         {
             rb_Camera.constraints = RigidbodyConstraints.FreezePosition;
-            rb_Camera.isKinematic = true;
             rb_Camera.useGravity = false;
 
+            //Hace que no puedas lanzar mas de una camara.
             throwCamera.cameraFixed = true;
+
+            //Saca la normal de el punto de colision.
+            calculeAngle(collision.GetContact(collision.contactCount - 1).normal);
         }
-        else // Mas adelante implementar animación de destruir camara
+        else // Mas adelante implementar animación de destruir camara.
             Destroy(gameObject);
+    }
+
+    void calculeAngle(Vector3 contactPoint)
+    {
+        //calcula el angulo inicial de la camara.
+        
+        virtualCamera.transform.rotation = Quaternion.FromToRotation(Vector3.forward, contactPoint) * virtualCamera.transform.rotation;
+        initialCameraRotation = virtualCamera.transform.rotation.y;
+        if (contactPoint.z < 0)
+            virtualCamera.transform.Rotate(0, 0, 180);
+
+        min = initialCameraRotation - 0.6f;
+        max = initialCameraRotation + 0.6f;
+
     }
 }
--- Project Stealth/Assets/Scripts/throwCamera.cs
+++ Project Stealth/Assets/Scripts/throwCamera.cs
@@ -8,25 +8,53 @@ public class throwCamera : MonoBehaviour
     public GameObject attachableCamera;
     private GameObject thisAttachableCamera;
 
-    public GameObject camera;
+    public GameObject cameraPlayer;
 
     public LayerMask layerMask;
 
     public static bool cameraFixed = false;
+    private static bool lookingCamera = false;
+
+    private float mouseInput;
+    private Vector3 lookhere;
 
     private void Update()
     {
-        if (Input.GetButtonDown("ThrowCamera") && !cameraFixed)
+        if (cameraFixed)
         {
-            thisAttachableCamera = Instantiate(attachableCamera, transform.position + Camera.main.transform.TransformDirection(transform.forward), Quaternion.identity);
-            thisAttachableCamera.GetComponent<Rigidbody>().AddForce(Camera.main.transform.TransformDirection(transform.forward) * 1000);
+            if (Input.GetButtonDown("ThrowCamera"))
+            {
+                cameraPlayer.SetActive(!cameraPlayer.activeSelf);
+                lookingCamera = !cameraPlayer.activeSelf;
+            }
+            if (lookingCamera)
+            {
+                mouseInput = Input.GetAxis("Mouse X");
+                lookhere = new Vector3(0, mouseInput, 0);
+                if (thisAttachableCamera.transform.rotation.y > colliderCamera.min && mouseInput < 0)
+                {
+                    thisAttachableCamera.transform.Rotate(lookhere);
+                }
+                else if(thisAttachableCamera.transform.rotation.y < colliderCamera.max && mouseInput > 0)
+                {
+                    thisAttachableCamera.transform.Rotate(lookhere);
+                }
+            }
         }
-
-        if (Input.GetButtonDown("ThrowCamera") && cameraFixed)
+        else
         {
-            camera.SetActive(!camera.activeSelf);
+            if (Input.GetButtonDown("ThrowCamera"))
+            {
+                thisAttachableCamera = Instantiate(attachableCamera, transform.position + Camera.main.transform.TransformDirection(transform.forward), Quaternion.identity);
+                thisAttachableCamera.GetComponent<Rigidbody>().AddForce(Camera.main.transform.TransformDirection(transform.forward) * 1000);
+            }
         }
+    }
 
-        //Falta hacer que el ppersonaje tenga que ir a recoger la camara para volverla a usar.
+
+    //Getter para poder saber si estás en la camara o no.
+    public static bool getLookingCamera()
+    {
+        return lookingCamera;
     }
 }
