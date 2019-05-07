7721680298b09355248bfd46f1fff96f96e617c3
Guillem Ortega <guillem212@hotmail.es>
2019-05-07 16:42:01 +0200

    dsfdsf

---
+++ Project Stealth/Assets/Scripts/PatrolEnemyAI.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+
+
+public enum EnemyState
+{
+    PATROL,
+    SEARCHING,
+    ATTACKING
+}
+
+
+public class PatrolEnemyAI : MonoBehaviour
+{
+
+
+    public EnemyState enemyState;
+    private NavMeshAgent agent;
+    private Vector3 positionPlayer;
+    public Image image;
+
+    private Vector3 playerPos;
+
+    public Sprite imageStart;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        enemyState = EnemyState.PATROL;
+        image = GetComponentInChildren<Image>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(enemyState == EnemyState.SEARCHING)
+        {
+            /*positionPlayer = Vector3.Lerp(transform.position, StealthBehaviour.lastpositionKnown, Time.deltaTime);
+            transform.LookAt(positionPlayer);*/
+            StartCoroutine(goToPosition());
+        }
+        else if(enemyState == EnemyState.ATTACKING)
+        {
+            agent.SetDestination(playerPos);
+        }
+    }
+
+    IEnumerator goToPosition()
+    {
+        image.sprite = imageStart;
+        yield return new WaitForSeconds(1f);
+        agent.SetDestination(StealthBehaviour.lastpositionKnown);
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerPos = other.transform.position;
+            enemyState = EnemyState.ATTACKING;
+        }
+    }
+
+}
---
+++ Project Stealth/Assets/Scripts/StealthBehaviour.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StealthBehaviour : MonoBehaviour
+{
+
+    private SphereCollider colliderSphere;
+    private float updateVel = 2f;
+
+    private CharacterController charController;
+
+    private float amountOfSound = 0.5f;
+    private float velocityOfPlayer = 0;
+
+
+    private bool wasGrounded = true;
+    public static Vector3 lastpositionKnown;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        colliderSphere = GetComponent<SphereCollider>();
+        colliderSphere.radius = amountOfSound;
+        charController = GetComponentInParent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        updateCollider();
+    }
+
+    private void updateCollider()
+    {
+        velocityOfPlayer = (charController.velocity.x + charController.velocity.z) * 2 ;
+
+        if (velocityOfPlayer != 0)
+        {
+            amountOfSound = Mathf.Abs(velocityOfPlayer);
+        }
+        else
+        {
+           amountOfSound = 0.5f;
+        }
+
+        if (charController.isGrounded && !wasGrounded)
+        {
+            amountOfSound += 100;
+        }
+        wasGrounded = charController.isGrounded;
+
+        colliderSphere.radius = Mathf.Lerp(colliderSphere.radius, amountOfSound, Time.deltaTime * updateVel);
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            if (other.gameObject.GetComponent<PatrolEnemyAI>().enemyState == EnemyState.PATROL)
+            {
+                lastpositionKnown = transform.position;
+                other.gameObject.GetComponent<PatrolEnemyAI>().enemyState = EnemyState.SEARCHING;
+            }
+            else if (other.gameObject.GetComponent<PatrolEnemyAI>().enemyState == EnemyState.SEARCHING)
+            {
+                other.gameObject.GetComponent<PatrolEnemyAI>().enemyState = EnemyState.ATTACKING;
+                //Aqui va el ataque
+            }
+        }
+    }
+}
