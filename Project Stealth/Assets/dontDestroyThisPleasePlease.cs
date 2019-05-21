604e8f548edaee29fd5545c8476f76d661686dd7
al361691 <44824232+al361691@users.noreply.github.com>
2019-05-21 12:32:39 +0200

    musiquelo

---
+++ Project Stealth/Assets/dontDestroyThisPleasePlease.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dontDestroyThisPleasePlease : MonoBehaviour
+{
+    private GameObject instance;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if(instance != null)
+        {
+            Destroy(this.gameObject);
+        }
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
